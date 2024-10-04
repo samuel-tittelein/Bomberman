0aacdfff5b72ffa92c3ccc5b3b5ee9e36359a226
Clement Goustiaux <clement_goustiaux@ens.univ-artois.fr>
2024-10-04 14:57:47 +0200

    Méthodes pour le déplacement sur la carte

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Enemy.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Enemy.java
@@ -3,8 +3,11 @@ package fr.univartois.butinfo.r304.bomberman.model.movables;
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
 import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+import java.util.Random;
 
 public class Enemy extends AbstractMovable {
+
+  private static final Random RANDOM = new Random();
 
   /**
    * Crée une nouvelle instance d'Enemy.
@@ -32,5 +35,33 @@ public class Enemy extends AbstractMovable {
   @Override
   public void hitEnemy() {
     explode();
+  }
+
+  @Override
+  public boolean move(long delta) {
+    boolean moved = super.move(delta);
+    if (!moved) {
+      changeDirectionRandomly();
+    }
+    return moved;
+  }
+
+  private void changeDirectionRandomly() {
+    // 50% de chance de changer la vitesse horizontale
+    if (RANDOM.nextBoolean()) {
+      setHorizontalSpeed(getRandomSpeed());
+    }
+    // 50% de chance de changer la vitesse verticale
+    if (RANDOM.nextBoolean()) {
+      setVerticalSpeed(getRandomSpeed());
+    }
+  }
+
+  private double getRandomSpeed() {
+    double speed;
+    do {
+      speed = -100 + (200) * RANDOM.nextDouble();
+    } while (speed > -10 && speed < 10);
+    return speed;
   }
 }
