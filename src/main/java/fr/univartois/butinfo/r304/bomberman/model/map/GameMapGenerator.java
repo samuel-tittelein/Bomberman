da0985808653be84499ba7a838894f005bdceffe
Tittelein Samuel <samuel_tittelein@ens.univ-artois.fr>
2024-10-19 00:02:21 +0200

    :sparkles: changement de la methode de création des murs

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/map/GameMapGenerator.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/map/GameMapGenerator.java
@@ -1,13 +1,15 @@
 package fr.univartois.butinfo.r304.bomberman.model.map;
 
 
+import fr.univartois.butinfo.r304.bomberman.model.map.walls.State.State;
 import fr.univartois.butinfo.r304.bomberman.model.map.walls.Wall;
 import fr.univartois.butinfo.r304.bomberman.view.SpriteStore;
 
 public class GameMapGenerator {
 
     SpriteStore ss = new SpriteStore();
-    Cell wall = new Cell(new Wall(ss.getSprite("wall")));
+    Cell wall = new Cell(new Wall(State.DEGRADED));
+    Cell indestuctibleWall = new Cell(new Wall(State.INDESTRUCTIBLE));
     Cell lawn = new Cell(ss.getSprite("lawn"));
 
     public GameMap fillMap(GameMap map) {
@@ -17,7 +19,7 @@ public class GameMapGenerator {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1) { // Bordures
-                    map.setAt(i, j, wall);
+                    map.setAt(i, j, indestuctibleWall);
                 } else if (Math.random() < 0.2) { // Mur aléatoire avec probabilité de 20%
                     map.setAt(i, j, wall);
                 } else {
