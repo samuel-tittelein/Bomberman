2f2da0db51fc13e44d00249201193d6db2900adb
Tittelein Samuel <samuel_tittelein@ens.univ-artois.fr>
2024-10-18 23:25:00 +0200

    :sparkles: ajout de la classe abstraite AbstractWallState

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/map/walls/State/AbstractWallState.java
@@ -0,0 +1,23 @@
+package fr.univartois.butinfo.r304.bomberman.model.map.walls.State;
+
+import fr.univartois.butinfo.r304.bomberman.model.map.walls.IWallState;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+import fr.univartois.butinfo.r304.bomberman.view.SpriteStore;
+
+abstract class AbstractWallState implements IWallState {
+    public static SpriteStore sp = new SpriteStore();
+    protected Sprite sprite;
+
+    public AbstractWallState(String identifier) {
+        sprite = sp.getSprite(identifier);
+    }
+
+    @Override
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+}
