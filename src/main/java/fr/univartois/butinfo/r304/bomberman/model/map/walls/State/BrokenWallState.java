9bb888218e93af651ba65b77476a1137665fe6cc
Tittelein Samuel <samuel_tittelein@ens.univ-artois.fr>
2024-10-18 23:10:07 +0200

    :sparkles: ajout de l'état BrokenWallState

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/map/walls/State/BrokenWallState.java
@@ -0,0 +1,10 @@
+package fr.univartois.butinfo.r304.bomberman.model.map.walls.State;
+
+import fr.univartois.butinfo.r304.bomberman.model.map.walls.IWallState;
+
+public class BrokenWallState implements IWallState {
+
+    public IWallState nextState() {
+        return null;
+    }
+}
