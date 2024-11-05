fffdaaa40acf5b835a36dfa509ff9613ecd3650d
Clement Goustiaux <clement_goustiaux@ens.univ-artois.fr>
2024-11-05 14:37:33 +0100

    🖼️ Décorateur pour les PV

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/MovableHPDecorator.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/MovableHPDecorator.java
@@ -1,4 +1,40 @@
 package fr.univartois.butinfo.r304.bomberman.model;
 
-public class MovableHPDecorator {
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public abstract class MovableHPDecorator implements IMovable {
+
+  private final IMovable movable;
+  private final BombermanGame game;
+
+  private final IntegerProperty hp;
+
+  protected MovableHPDecorator(int hp, BombermanGame game, IMovable movable) {
+    this.hp = new SimpleIntegerProperty(hp);
+    this.movable = movable;
+    this.game = game;
+  }
+
+  protected MovableHPDecorator(IMovable movable, BombermanGame game) {
+    this.hp = new SimpleIntegerProperty(1);
+    this.movable = movable;
+    this.game = game;
+  }
+
+  public BombermanGame getGame() {
+    return game;
+  }
+
+  public int getHp() {
+    return hp.get();
+  }
+
+  public void decreaseHp() {
+    hp.setValue(hp.getValue() - 1);
+  }
+
+  public IMovable getMovable() {
+    return movable;
+  }
 }
